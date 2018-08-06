0ae0e770f97fc90b3d7dd4f7a2d8e11ada9404ec
JPRichings <james.richings@hotmail.com>
2018-08-06 16:04:47 +0100

    resize of evectmp

--- extras/Hadrons/EigenPack.hpp
+++ extras/Hadrons/EigenPack.hpp
@@ -73,6 +73,7 @@ public:
     {
         eval.resize(size);
         evec.resize(size, grid);
+        evectmp.resize(size,grid);
     }
 
     virtual void read(const std::string fileStem, const bool multiFile, const int traj = -1)
@@ -182,7 +183,7 @@ protected:
             //RealF tmp = (RealF) eval[k];
             //eval[k] = (RealD) tmp;
             // convert the eigen vectors to single precision
-            localConvertJamesR(evec,evectmp);
+            localConvertJamesR(evec, evectmp);
             LOG(Message) << "double" << norm2(evec) << std::endl;
             LOG(Message) << "single" << norm2(evectmp) << std::endl;
             //evec[k] = evec[k] - evectmp[k];
