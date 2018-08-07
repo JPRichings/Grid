58c71dc894fffc37baa93f28eaeee77032449053
JPRichings <james.richings@hotmail.com>
2018-08-07 16:17:58 +0100

    more peek poke

--- lib/lattice/Lattice_transfer.h
+++ lib/lattice/Lattice_transfer.h
@@ -364,6 +364,41 @@ void localConvert(const Lattice<vobj> &in,Lattice<vvobj> &out)
     pokeLocalSite(ss,out,lcoor);
   }
 }
+
+template<class vobj,class vvobj>
+void localConvertJPR(const Lattice<vobj> &in,Lattice<vvobj> &out)
+{
+  typedef typename vobj::scalar_object sobj;
+  typedef typename vvobj::scalar_object ssobj;
+
+  GridBase *ig = in._grid;
+  GridBase *og = out._grid;
+
+  int ni = ig->_ndimension;
+  int no = og->_ndimension;
+
+  assert(ni == no);
+
+  for(int d=0;d<no;d++){
+    assert(ig->_processors[d]  == og->_processors[d]);
+    assert(ig->_ldimensions[d] == og->_ldimensions[d]);
+    assert(ig->lSites() == og->lSites());
+  }
+
+  parallel_for(int idx=0;idx<ig->lSites();idx++){
+    sobj s;
+    ssobj ss;
+
+    Complex buf;
+
+    std::vector<int> lcoor(ni);
+    ig->LocalIndexToLocalCoor(idx,lcoor);
+    peekLocalSite(buf,peekSpin(peekColour(in,0),0),lcoor);
+    //ss=s;
+    std::cout << GridLogMessage << "HiThisISBUF:" << buf << std::endl;
+    //pokeLocalSite(ss,out,lcoor);
+  }
+}
 //dirty hack to convert eigen packs from double to single precision.
 /*template<class vobj,class vvobj>
 void localConvertJamesR(const Lattice<vobj> &in,Lattice<vvobj> &out)
